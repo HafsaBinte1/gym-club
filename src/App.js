1e62fda262189f10eab77833137ac47def43ea45
unknown <hafsabinteharun101@gmail.com>
2022-11-12 02:29:27 +0600

    ten commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 
 import Gym from './Component/Gym/Gym';
+import Question from './Component/Question/Question';
 
 function App() {
   return (
     <div className="">
    <Gym></Gym>
+   <Question></Question>
     </div>
   );
 }
